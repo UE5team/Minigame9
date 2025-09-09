f1fb918074277f2785a8ad37d7985df341e0da6b
相原 <sora041219@gmail.com>
2025-09-09 13:58:12 +0900

    確認

--- Source/Trial_Lesson/CountDownWidget.cpp
+++ Source/Trial_Lesson/CountDownWidget.cpp
@@ -1,69 +1,65 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CountDownWidget.h"
 #include "Components/TextBlock.h"
 #include "TimerManager.h"
-#include "Kismet/GameplayStatics.h"
-#include "Blueprint/WidgetBlueprintLibrary.h"
-
-void UCountDownWidget::StartCountdown( ) {
-	cressent_count = 3;
-	UpdateCountdown( );
-	GetWorld( )->GetTimerManager( ).SetTimer(countdowntimer, this, &UCountDownWidget::UpdateCountdown, 1.0f, true);
-	UE_LOG(LogTemp, Warning, TEXT("StartCount True"));
-}
+#include "Engine/Engine.h"
 
 void UCountDownWidget::NativeConstruct( ) {
-	Super::NativeConstruct( );
-	cressent_count = 3;
+    Super::NativeConstruct( );
+    cressent_count = 3;
 }
 
-UCountDownWidget* UCountDownWidget::ShowCountdown(UObject* WorldContextObject) {
-	if (!WorldContextObject)
-	{
-		return nullptr;
-	}
+void UCountDownWidget::StartCountdown( ) {
+    cressent_count = 3;
+    UpdateCountdown( );
 
-	UWorld* World = GEngine->GetWorldFromContextObjectChecked(WorldContextObject);
+    // 1秒ごとにUpdateCountdownを呼ぶタイマー
+    if ( GetWorld( ) ) {
+        GetWorld( )->GetTimerManager( ).SetTimer( countdowntimer, this, &UCountDownWidget::UpdateCountdown, 1.0f, true );
+    }
 
-	if (!World)
-	{
-		return nullptr;
-	}
-
-	UCountDownWidget* Widget = CreateWidget<UCountDownWidget>(World, UCountDownWidget::StaticClass( ));
-
-	if (Widget)
-	{
-		Widget->AddToViewport(100);
-		Widget->StartCountdown( );
-	}
-
-	return Widget;
+    UE_LOG( LogTemp, Warning, TEXT( "StartCountdown called" ) );
 }
 
-void UCountDownWidget::UpdateCountdown() 
-{
-	
-	if (!CountdownText)
-	{
-		return;
-	}
+void UCountDownWidget::UpdateCountdown( ) {
+    if ( !CountdownText ) {
+        UE_LOG( LogTemp, Warning, TEXT( "CountdownText is nullptr!" ) );
+        return; // nullptrなら何もしない
+    } else {
+        UE_LOG( LogTemp, Warning, TEXT( "CountdownText is valid: %d" ), cressent_count );
+    }
 
-	if (cressent_count > 0)
-	{
-		CountdownText->SetText(FText::AsNumber(cressent_count));
-		cressent_count--;
-		UE_LOG(LogTemp, Warning, TEXT("UpdateCountdown True"));
-	}
-	else
-	{
-		CountdownText->SetText(FText::FromString(TEXT("GO!")));
-		GetWorld( )->GetTimerManager( ).ClearTimer(countdowntimer);
-		GetWorld( )->GetTimerManager( ).SetTimerForNextTick([ this ] ( )
-															{
-																this->RemoveFromParent( );
-															});
-	}
+    if ( cressent_count > 0 ) {
+        CountdownText->SetText( FText::AsNumber( cressent_count ) );
+        cressent_count--;
+    } else {
+        CountdownText->SetText( FText::FromString( TEXT( "GO!" ) ) );
+        GetWorld( )->GetTimerManager( ).ClearTimer( countdowntimer );
+        GetWorld( )->GetTimerManager( ).SetTimerForNextTick( [ this ] ( ) {
+            this->RemoveFromParent( );
+        } );
+    }
 }
+
+UCountDownWidget* UCountDownWidget::ShowCountdown( UObject* WorldContextObject, TSubclassOf<UCountDownWidget> WidgetClass ) {
+    if ( !WorldContextObject || !WidgetClass ) {
+        UE_LOG( LogTemp, Warning, TEXT( "ShowCountdown: Invalid WorldContextObject or WidgetClass" ) );
+        return nullptr;
+    }
+
+    // 安全にWorldを取得（nullptrでも落ちない）
+    UWorld* World = GEngine ? GEngine->GetWorldFromContextObject( WorldContextObject, EGetWorldErrorMode::LogAndReturnNull ) : nullptr;
+    if ( !World ) {
+        UE_LOG( LogTemp, Warning, TEXT( "ShowCountdown: World is nullptr" ) );
+        return nullptr;
+    }
+
+    // Widgetを生成
+    UCountDownWidget* Widget = CreateWidget<UCountDownWidget>( World, WidgetClass );
+    if ( Widget ) {
+        Widget->AddToViewport( 100 );
+        Widget->StartCountdown( );
+    }
+
+    return Widget;
+}
+
